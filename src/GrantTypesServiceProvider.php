d1b9149e05d0c6423d58f1c366d35d6ce564d613
Karim Slimani <karim@playmedia.fr>
2019-05-16 12:21:48 +0200

    feat add grant types service provider

---
+++ src/GrantTypesServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sk\Passport;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Bridge\RefreshTokenRepository;
+use Laravel\Passport\Passport;
+use League\OAuth2\Server\AuthorizationServer;
+
+class GrantTypesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/passportgrant.php' => $this->app->configPath().'/passportgrant.php',
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->app
+            ->afterResolving(AuthorizationServer::class, function (AuthorizationServer $server) {
+                $this->makeGrants($server);
+            });
+    }
+
+    public function makeGrants(AuthorizationServer $server)
+    {
+        $grants = $this->app->config->get('passportgrant.grants', []);
+
+        foreach ($grants as $grantType => $userResolverClass) {
+            $grant = new Grant(
+                $grantType,
+                $this->app->make($userResolverClass),
+                $this->app->make(RefreshTokenRepository::class)
+            );
+            $grant->setRefreshTokenTTL(Passport::refreshTokensExpireIn());
+            $server->enableGrantType($grant, Passport::tokensExpireIn());
+        }
+    }
+}
